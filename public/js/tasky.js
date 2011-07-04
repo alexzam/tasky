7103f1aeca1145f7904f9bd4b9136756c320a61e
AlexZam <alexzzam@gmail.com>
2011-07-04 19:03:05 +0400

    Added saving of top tasks editing events.

--- public/js/tasky.js
+++ public/js/tasky.js
@@ -1,17 +1,85 @@
+function Task(id, parentId) {
+    this.id = id;
+    this.label = '';
+    this.x = 100;
+    this.y = 100;
+    this.marked = false;
+    this.page = false;
+    this.parent = parentId;
+    this.subs = [];
+}
+
+Task.prototype.merge = function(source) {
+    for (i in source) {
+        if (i == 'id') continue;
+        if (i == 'subs') {
+            // TODO Merge subtasks
+            continue;
+        }
+
+        this[i] = source[i];
+    }
+};
+
 var tasky = {
     newid: 1,
     outBuffer: [],
 
+    addTopTaskButton: function() {
+        var newt = new Task(-1, global.rootId);
+        newt.label = 'New Task';
+        var tid = tasky.addTopTask(newt);
+        tasky.addInsertToBuffer(newt, tid);
+        return false;
+    },
+
+    addTopTask: function(task) {
+        var marked = (task.marked) ? ' marked' : 'unmarked';
+        var tid = 't' + tasky.newid;
+
+        var item = $('#toptask-template')
+            .clone()
+            .attr('id', tid)
+            .attr('tid', task.id).addClass(marked)
+            .removeClass('hidden')
+            .css({left:task.x + 'px', top:task.y + 'px'})
+            .appendTo('.playground')
+            .draggable({distance:3})
+            .bind('dragstop', {tid:task.id}, tasky.topTaskDrag)
+            .mouseout();
+        item.children('span')
+            .text(task.label);
+        item.data('t', task);
+        tasky.newid++;
+        for (var i in task.subs) {
+            tasky.addSubTask(item, false, task.subs[i]);
+        }
+        return tid;
+    },
+
     topTaskDrag: function(e, ui) {
-        // TODO Review
-        $('#msg').text('ID: ' + e.data.tid + ', X: ' + ui.position.left + ', y: ' + ui.position.top);
-        $(e.target).click();
+        var item = $(e.target);
+
+        var task = item.data().t;
+        task.x = ui.position.left;
+        task.y = ui.position.top;
+
+        $('#msg').text('ID: ' + e.data.tid + ', X: ' + task.x + ', y: ' + task.y);
+        item.click();
+
+        tasky.addUpdateToBuffer(task);
     },
 
     saveLabel: function(e) {
         var inp = $(this);
-        inp.closest('.task').find('.title').text(inp.val()).show();
+        var val = inp.val();
+        var item = inp.closest('.task');
+        item.find('.title').text(val).show();
         inp.remove();
+        var task = item.data().t;
+        task.label = val;
+
+        tasky.addUpdateToBuffer(task);
     },
 
     labelKeyUp: function(e) {
@@ -22,17 +90,16 @@ var tasky = {
         var item = $(this).closest('.task');
         var txt = item.find('.title').hide().text();
         item.prepend($('<input>')
-                .attr('type', 'text')
-                .val(txt)
-                .blur(tasky.saveLabel)
-                .keyup(tasky.labelKeyUp)
-                );
+            .attr('type', 'text')
+            .val(txt)
+            .blur(tasky.saveLabel)
+            .keyup(tasky.labelKeyUp)
+        );
         item.children('input').focus();
     },
 
     topTaskDeselect: function(e) {
-        $(this).removeClass('selected')
-                .find('.hiding').hide();
+        $(this).removeClass('selected').find('.hiding').hide();
         $(this).find('.hiding2').hide();
     },
 
@@ -45,8 +112,8 @@ var tasky = {
 
     topTaskSelect: function(e) {
         $(this)
-                .addClass('selected')
-                .find('.hiding').show();
+            .addClass('selected')
+            .find('.hiding').show();
         $(this).find('.hiding2').show();
     },
 
@@ -55,157 +122,135 @@ var tasky = {
         $(this).children('.tasklist').children('.hiding2').removeClass('hide');
     },
 
-    addTopTask: function(task) {
-        var marked = (task.marked) ? ' marked' : 'unmarked';
-        var tid = 't' + tasky.newid;
-
-        var item = $('#toptask-template')
-                .clone()
-                .attr('id', tid)
-                .attr('tid', task.id)
-                .addClass(marked)
-                .removeClass('hidden')
-                .offset({left:task.x, top:task.y})
-                .appendTo('.playground')
-                .draggable({distance:3})
-                .bind('dragstop', {tid:task.id}, tasky.topTaskDrag)
-                .mouseout();
-        item.children('span')
-                .text(task.label);
-        item.data('t', task);
-        tasky.newid++;
-        for(var i in task.subs){
-            tasky.addSubTask(item, false, task.subs[i]);
-        }
-        return tid;
-    },
-
     addSubTask: function(item, manual, task) {
         var marked = (task.marked) ? ' marked' : 'unmarked';
         var newi = $('#subtask-template')
-                .clone()
-                .attr('id', 't' + task.id)
-                .addClass(marked);
+            .clone()
+            .attr('id', 't' + task.id)
+            .addClass(marked);
         item.children('.tasklist').children().last().before(newi);
         newi.children('.title')
-                .text(task.label);
+            .text(task.label);
         item.children('.but-fold')
-                .show();
+            .show();
         newi.data('t', task);
-        if(manual) newi.children('.title').dblclick();
-        else for(var i in task.subs){
+        if (manual) newi.children('.title').dblclick();
+        else for (var i in task.subs) {
             tasky.addSubTask(newi, false, task.subs[i]);
         }
+        // TODO Add update sending
     },
 
     newTaskClick: function(e) {
         var task = $(this).parents('.task').first();
         $('#msg').text('New task within ' + task.attr('tid'));
         var id = tasky.newid;
-        var newt = {id:"t"+id, label:"New Subtask", marked:false,sub:[]};
-        tasky.newid++; // TEMP
+        var newt = {id:"t" + id, label:"New Subtask", marked:false,sub:[]};
+        tasky.newid++;
         tasky.addSubTask(task, true, newt);
         tasky.addInsertToBuffer(newt);
         $(this).parents('.tasklist').removeClass('hiding2');
     },
-    
+
     markTask: function(e) {
-        $(this).closest('.task')
-                .toggleClass('marked')
-                .toggleClass('unmarked');
+        var item = $(this).closest('.task')
+            .toggleClass('marked')
+            .toggleClass('unmarked');
+        var task = item.data().t;
+        task.marked = (item.hasClass('marked'));
+
+        tasky.addUpdateToBuffer(task);
+
         return false;
     },
 
     foldList: function(e) {
         var tasklist = $(this).closest('.task').children('.tasklist');
-        
+
         if ($(this).hasClass('minus')) {
             $(this)
-                    .removeClass('minus')
-                    .text('+');
+                .removeClass('minus')
+                .text('+');
             tasklist.hide();
         } else {
             $(this)
-                    .addClass('minus')
-                    .text('-');
+                .addClass('minus')
+                .text('-');
             tasklist.show();
         }
     },
 
-    addInsertToBuffer: function(task, tid){
+    addInsertToBuffer: function(task, tid) {
         task.tid = tid;
         tasky.outBuffer.push({act:'i', task: task});
     },
 
-    addUpdateToBuffer: function(task){
-        tasky.outBuffer.push({act:'u', id:task.tid, task: task});
+    addUpdateToBuffer: function(task) {
+        tasky.outBuffer.push({act:'u', id:task.id, task: task});
     },
 
-    addDeleteToBuffer: function(id){
+    addDeleteToBuffer: function(id) {
         tasky.outBuffer.push({act:'d', id: id});
     },
 
-    sendUpdates: function(){
+    sendUpdates: function() {
         var data = [];
 
-        while(tasky.outBuffer.length > 0){
+        while (tasky.outBuffer.length > 0) {
             // Do not post update events for task that was not yet added
-            if (tasky.outBuffer[0].act == 'u' && tasky.outBuffer[0].id.charAt(0) == 't') break;
+            if (tasky.outBuffer[0].act == 'u' && tasky.outBuffer[0].id == -1) break;
             data.push(tasky.outBuffer.shift());
         }
 
-        if(data.length > 0){
+        if (data.length > 0) {
             data = {data: data};
-            console.dir(data);
             $.post(
-                'play/update', 
-                data, 
-                function(data, status){
-                    for(i in data){
+                'play/update',
+                data,
+                function(data, status) {
+                    for (i in data) {
                         var ditem = data[i];
-                        if(ditem == null) continue;
+                        if (ditem == null) continue;
 
-                        console.log('Incoming update of type '+ditem.type);
-                        if(ditem.type == 'i'){
-                            console.log('Changing id '+ditem.o+' to '+ditem.n);
-                            $('.playground')
-                                .find('.task[id='+ditem.o+']')
-                                .attr('tid', ditem.n);
-                            for(j in tasky.outBuffer){
-                                if(tasky.outBuffer[j].id == ditem.o) tasky.outBuffer[j].id = ditem.n;
+                        console.log('Incoming update of type ' + ditem.type);
+                        if (ditem.type == 'i') {
+                            console.log('Changing id ' + ditem.o + ' to ' + ditem.n);
+                            var item = $('.playground')
+                                .find('.task[id=' + ditem.o + ']');
+                            item.attr('tid', ditem.n);
+                            item.data().t.id = ditem.n;
+                            for (j in tasky.outBuffer) {
+                                if (tasky.outBuffer[j].id == ditem.o) tasky.outBuffer[j].id = ditem.n;
                             }
                         }
                     }
-                }, 
-                'json'                                                                              
-            );                                                                                   
-        } else console.log("No data");                                                           
-                                                                                                 
-        setTimeout('tasky.sendUpdates()', 500);                                                  
-    }                                                                                            
-};                                                                                                
-                                                                                                 
-// Initialization                                                                                 
-$(function() {                                                                                    
-    $('.playground')                                                                           
-            .delegate('.toptask', 'mouseenter', tasky.topTaskSelect)                           
-            .delegate('.toptask', 'mouseleave', tasky.topTaskDeselect)                         
-            .delegate('.task', 'mouseenter', tasky.taskSelect)                                 
-            .delegate('.task', 'mouseleave', tasky.taskDeselect);
+                },
+                'json'
+            );
+        } else console.log("No data");
 
-    $.getJSON('play/get-task-page', {id:global.rootId}, function(data){
+        setTimeout('tasky.sendUpdates()', 500);
+    }
+};
+
+// Initialization                                                                                 
+$(function() {
+    $('.playground')
+        .delegate('.toptask', 'mouseenter', tasky.topTaskSelect)
+        .delegate('.toptask', 'mouseleave', tasky.topTaskDeselect)
+        .delegate('.task', 'mouseenter', tasky.taskSelect)
+        .delegate('.task', 'mouseleave', tasky.taskDeselect);
+
+    $.getJSON('play/get-task-page', {id:global.rootId}, function(data) {
         for (var i in data) {
-            tasky.addTopTask(data[i]);
+            var ditem = data[i];
+            var item = new Task(ditem.id, global.rootId);
+            item.merge(ditem);
+            tasky.addTopTask(item);
         }
     });
-    
-    $('#but-topadd').click(function() {
-        var id = tasky.newid;
-        var newt = {id:-1, label:"New Task", x:100, y:100,marked:false,sub:[],parent:global.rootId};
-        var tid = tasky.addTopTask(newt);
-        tasky.addInsertToBuffer(newt, tid);
-        return false;
-    });
+
+    $('#but-topadd').click(tasky.addTopTaskButton);
     $('.toptask').mouseleave();
     // TODO Deletion
     //    $('#but-locdel').click(function() {
